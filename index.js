74ff9c66022905780c0ccd2d27500152cbfb9549
Abraham JV <ingabrahamjv@gmail.com>
2024-04-26 14:13:47 -0600

    inicio
    new

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+// Configuración y rutas de tu aplicación Express
+
+app.listen(3000, () => {
+  console.log('Servidor Express iniciado en el puerto 3000');
+});
